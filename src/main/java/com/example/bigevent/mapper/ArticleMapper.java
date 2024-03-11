8f350a395f29d898f2457f502fc24d985b2e8490
xi <chengsx2518@gmail.com>
2024-03-11 12:58:55 +0800

    article add

--- src/main/java/com/example/bigevent/mapper/ArticleMapper.java
+++ src/main/java/com/example/bigevent/mapper/ArticleMapper.java
@@ -1,6 +1,7 @@
 package com.example.bigevent.mapper;
 
 import com.example.bigevent.pojo.Article;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
@@ -9,5 +10,8 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface ArticleMapper {
+    //新增
+    @Insert("insert into article(title,content,cover_img,state,category_id,create_user,create_time,update_time) " +
+            "values (#{title},#{content},#{coverImg},#{state},#{categoryId},#{createUser},#{createTime},#{updateTime})")
     void add(Article article);
 }
--- src/main/java/com/example/bigevent/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/example/bigevent/service/impl/ArticleServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.bigevent.service.impl;
 import com.example.bigevent.mapper.ArticleMapper;
 import com.example.bigevent.pojo.Article;
 import com.example.bigevent.service.ArticleService;
+import com.example.bigevent.utils.ThreadLocalUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 /**
  * @author xi
@@ -20,6 +22,10 @@ public class ArticleServiceImpl implements ArticleService {
     public void add(Article article) {
         //补充属性值
         article.setCreateTime(LocalDateTime.now());
+        article.setUpdateTime(LocalDateTime.now());
+        Map<String,Object> map = ThreadLocalUtil.get();
+        Integer userId = (Integer) map.get("id");
+        article.setCreateUser(userId);
         articleMapper.add(article);
     }
 }
